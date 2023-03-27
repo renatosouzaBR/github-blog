f88ef7c67ce228265de1d7aa346cd7fa1dc00851
Renato Souza <natoicm@gmail.com>
2023-03-27 16:47:05 -0300

    chore: add default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyles } from './styles/global'
+import { DefaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <GlobalStyles />
+
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  :focus {
+    outline: transparent !important;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.blue};
+  }
+
+  body {
+    background-color: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, text-area, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1.6rem;
+  }
+`
